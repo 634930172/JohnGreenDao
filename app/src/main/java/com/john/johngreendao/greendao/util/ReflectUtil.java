51524184b87738ddb01ed11a6a36ff17ab404fe1
John <li.yang@ubtrobot.com>
2019-09-05 16:12:51 +0800

    feat:test分支增加test5

--- app/src/main/java/com/john/johngreendao/greendao/util/ReflectUtil.java
+++ app/src/main/java/com/john/johngreendao/greendao/util/ReflectUtil.java
@@ -6,7 +6,7 @@ import com.john.johngreendao.greendao.entity.User;
 import java.lang.reflect.Field;
 
 /**
- * Author: John test的提交  test1 test2 test3 test4 test5
+ * Author: John test的提交  test1 test2 test3 test4 test5 test6
  * E-mail: 634930172@qq.com
  * Date: 2018/7/17 11:03
  * <p/>
